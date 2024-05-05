346e7bb03600d4e07df158997f7bfabd5ca0ddee
Zakaria ElQotbi <zakaria@elqotbi.com>
2024-05-06 00:51:05 +0200

    improve scripts

--- andromeda-nav-flight.py
+++ andromeda-nav-flight.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 # coding: utf-8
+import sys
 import matplotlib.pyplot as pyplot
 import numpy as np
-import sys
 from scipy.signal import savgol_filter
 
-#icm20984 is on 7,8,9 indexes.
+#icm20984-accel is on 7,8,9 indexes.
 accelStartIndex = 7
+#icm20984-gyro is on 10,11,12 indexes.
+gyroStartIndex = 10
+#icm20984-magn is on 13,14,15 indexes.
+magnStartIndex = 13
 factor = 0.6072237558389597 # factor got from "andromeda-stationary.py"
 accelName = "icm20984"
+
+#data = np.genfromtxt("samples/rocket-flight.csv", delimiter=" ", skip_header=1)
+data = np.genfromtxt("samples/rocket.csv", delimiter=" ", skip_header=1)
+timestamp = data[:, 21].astype(int)
+
+liftoffTs = 1919327
+startTsIndex = np.where(timestamp == liftoffTs)[0][0] - 1
+endTsIndex = np.size(timestamp) - 1
 
 try:
     if sys.argv[1] == "accel2":
@@ -16,6 +28,9 @@ try:
         accelStartIndex = 16
         factor = 0.010232154063793143
         accelName = "icm20600"
+    elif sys.argv[1] == "stationnary":
+        startTsIndex = 0
+        endTsIndex = np.where(timestamp == liftoffTs)[0][0] - 1
 except:
     pass
 
@@ -24,41 +39,60 @@ print("ANDRONAV: parsing accel {} data (idx:{}) using factor {}"
 
 #Parsing
 accelEndIndex = accelStartIndex + 3
-data = np.genfromtxt("samples/rocket.csv", delimiter=" ", skip_header=1)
+gyroEndIndex = gyroStartIndex + 3
+magnEndIndex = magnStartIndex + 3
 accelerometer = data[:,accelStartIndex:accelEndIndex]
-timestamp = (data[:, 21])/1000
+gyro = data[:,gyroStartIndex:gyroEndIndex]
+magn = data[:,magnStartIndex:magnEndIndex]
 
 #Correcting
 realAccel = accelerometer * factor
+realGyro = gyro
+realMagn = magn
 
-liftoffTs=1919327/1000
-startTsIndex=np.where(timestamp == liftoffTs)[0][0] - 1
-
-flightAccel=realAccel[startTsIndex:]
-flightTimestamp=timestamp[startTsIndex:]
+flightAccel=realAccel[startTsIndex:endTsIndex]
+flightGyro=realGyro[startTsIndex:endTsIndex]
+flightMagn=realMagn[startTsIndex:endTsIndex]
+flightTimestamp=timestamp[startTsIndex:endTsIndex]
 
 absFlightAccel = np.sqrt(np.square(flightAccel[:, 0]) + np.square(flightAccel[:, 1]) + np.square(flightAccel[:, 2]))
 
 #Smoothing
-smoothAbsFlightAccel = savgol_filter(absFlightAccel, 51, 3)
+#smoothAbsFlightAccel = savgol_filter(absFlightAccel, 51, 3)
 
 #Plotting
-num_axes = 2 # we will plot 'num_axes' subplots
+num_axes = 3 # we will plot 'num_axes' subplots
 figure, axes = pyplot.subplots(nrows=num_axes, sharex=True)
 figure.suptitle("Accel {} Analysis".format(accelName))
 index_axe = 0
 
-axes[index_axe].plot(flightTimestamp, smoothAbsFlightAccel, "tab:blue", label="SMOOTHABSFLIGHTACC")
-axes[index_axe].plot(flightTimestamp, absFlightAccel, "tab:red", label="ABSFLIGHTACC")
-axes[index_axe].set_ylabel("Abs Flight Accel")
-axes[index_axe].grid()
-axes[index_axe].legend()
-index_axe += 1
-
+#axes[index_axe].plot(flightTimestamp, smoothAbsFlightAccel, "tab:blue", label="SMOOTHABSFLIGHTACC")
+#axes[index_axe].plot(flightTimestamp, absFlightAccel, "tab:red", label="ABSFLIGHTACC")
+#axes[index_axe].set_ylabel("Abs Flight Accel")
+#axes[index_axe].grid()
+#axes[index_axe].legend()
+#index_axe += 1
+#
 axes[index_axe].plot(flightTimestamp, flightAccel[:, 0], "tab:red", label="FLIGHTACCX")
 axes[index_axe].plot(flightTimestamp, flightAccel[:, 1], "tab:green", label="FLIGHTACCY")
 axes[index_axe].plot(flightTimestamp, flightAccel[:, 2], "tab:blue", label="FLIGHTACCZ")
 axes[index_axe].set_ylabel("Flight Accel")
+axes[index_axe].grid()
+axes[index_axe].legend()
+index_axe += 1
+
+axes[index_axe].plot(flightTimestamp, flightGyro[:, 0], "tab:red", label="FLIGHTGYROX")
+axes[index_axe].plot(flightTimestamp, flightGyro[:, 1], "tab:green", label="FLIGHTGYROY")
+axes[index_axe].plot(flightTimestamp, flightGyro[:, 2], "tab:blue", label="FLIGHTGYROZ")
+axes[index_axe].set_ylabel("Flight Gyro")
+axes[index_axe].grid()
+axes[index_axe].legend()
+index_axe += 1
+
+axes[index_axe].plot(flightTimestamp, flightMagn[:, 0], "tab:red", label="FLIGHTMAGNX")
+axes[index_axe].plot(flightTimestamp, flightMagn[:, 1], "tab:green", label="FLIGHTMAGNY")
+axes[index_axe].plot(flightTimestamp, flightMagn[:, 2], "tab:blue", label="FLIGHTMAGNZ")
+axes[index_axe].set_ylabel("Flight Magn")
 axes[index_axe].grid()
 axes[index_axe].legend()
 index_axe += 1
--- andromeda-stationary.py
+++ andromeda-stationary.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # coding: utf-8
+import sys
 import matplotlib.pyplot as pyplot
 import pandas as pd
 import numpy as np
 from scipy import stats
-import sys
+from scipy.signal import savgol_filter
 
 #icm20984 is on 7,8,9 indexes.
 accelStartIndex = 7
@@ -23,7 +24,8 @@ print("ANDRONAV: parsing accel {} data (idx:{})"
 
 #Parsing
 accelEndIndex = accelStartIndex + 3
-data = np.genfromtxt("samples/rocket-stationary.csv", delimiter=" ", skip_header=1)
+#data = np.genfromtxt("samples/rocket-stationary.csv", delimiter=" ", skip_header=1)
+data = np.genfromtxt("samples/rocket.csv", delimiter=" ", skip_header=1)
 accelerometer = data[:,accelStartIndex:accelEndIndex]
 timestamp = (data[:, 21])/1000
 
@@ -50,19 +52,22 @@ print('median is {}, thus factor is {}'.format(median, factor))
 # now normalize the accelerometer data.
 cleanAccel = accelerometer*factor
 
+#Smoothing
+smoothAbsAccel = savgol_filter(cleanAbsAccel, 51, 3)
+
 #Plotting
-num_axes = 4 # we will plot 'num_axes' subplots
+num_axes = 3 # we will plot 'num_axes' subplots
 figure, axes = pyplot.subplots(nrows=num_axes, sharex=True)
 figure.suptitle("{} Accel Stationary Analysis".format(accelName))
 index_axe = 0
 
-axes[index_axe].plot(timestamp, accelerometer[:, 0], "tab:red", label="RAWACCX")
-axes[index_axe].plot(timestamp, accelerometer[:, 1], "tab:green", label="RAWACCY")
-axes[index_axe].plot(timestamp, accelerometer[:, 2], "tab:blue", label="RAWACCZ")
-axes[index_axe].set_ylabel("Raw Accel")
-axes[index_axe].grid()
-axes[index_axe].legend()
-index_axe += 1
+#axes[index_axe].plot(timestamp, accelerometer[:, 0], "tab:red", label="RAWACCX")
+#axes[index_axe].plot(timestamp, accelerometer[:, 1], "tab:green", label="RAWACCY")
+#axes[index_axe].plot(timestamp, accelerometer[:, 2], "tab:blue", label="RAWACCZ")
+#axes[index_axe].set_ylabel("Raw Accel")
+#axes[index_axe].grid()
+#axes[index_axe].legend()
+#index_axe += 1
 
 axes[index_axe].plot(timestamp, absAccel, "tab:red", label="ABS")
 axes[index_axe].set_ylabel("Abs Accel")
@@ -76,12 +81,18 @@ axes[index_axe].grid()
 axes[index_axe].legend()
 index_axe += 1
 
-axes[index_axe].plot(timestamp, cleanAccel[:, 0], "tab:red", label="CLEANACCX")
-axes[index_axe].plot(timestamp, cleanAccel[:, 1], "tab:green", label="CLEANACCY")
-axes[index_axe].plot(timestamp, cleanAccel[:, 2], "tab:blue", label="CLEANACCZ")
-axes[index_axe].set_ylabel("Clean Accel")
+axes[index_axe].plot(timestamp, smoothAbsAccel, "tab:red", label="SMOOTHABS")
+axes[index_axe].set_ylabel("Smooth Abs Accel")
 axes[index_axe].grid()
 axes[index_axe].legend()
 index_axe += 1
+
+#axes[index_axe].plot(timestamp, cleanAccel[:, 0], "tab:red", label="CLEANACCX")
+#axes[index_axe].plot(timestamp, cleanAccel[:, 1], "tab:green", label="CLEANACCY")
+#axes[index_axe].plot(timestamp, cleanAccel[:, 2], "tab:blue", label="CLEANACCZ")
+#axes[index_axe].set_ylabel("Clean Accel")
+#axes[index_axe].grid()
+#axes[index_axe].legend()
+#index_axe += 1
 
 pyplot.show()
